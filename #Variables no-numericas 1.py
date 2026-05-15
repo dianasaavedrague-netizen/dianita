ddbd5bd9ab5fc59c14c740c5696e896c7144f7b8
diana <diana.saavedra.gue@gmail.com>
2026-05-14 20:25:34 -0600

    Add files via upload

---
+++ #Variables no-numericas 1.py
@@ -0,0 +1,21 @@
+#Variables no-numericas
+#Diana Saavedra 
+#texto (Cadena de caracteres)
+
+nombre_usuario="Diana Saavedra "
+direccion_envio="calle morelos#123"
+
+#numeros
+cantidad_de_pizzas=2 #Entero
+precio_total=299.50 #Decimal/Flotante
+
+#Boleado(verdadero o Falso)
+esta_pagado=True
+
+#Mostrar datos 
+print("===RESUMEN DE PEDIDO===")
+print("Cliente:", nombre_usuario)
+print("Direccion:", direccion_envio)
+print("cantidad de pizzas:", cantidad_de_pizzas)
+print("Total a pagar:$", precio_total)
+print("¿Ya fue pagado?", esta_pagado)
